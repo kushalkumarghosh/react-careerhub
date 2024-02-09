ac3c2f2756ca0844c3a5a60f108423a44b7da529
kushalkumarghosh <cse.kushal1@gmail.com>
2024-02-09 19:24:31 +0600

    display applied jobs from stored ids

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,4 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../utility/localstorage";
 
 const JobDetails = () => {
     const jobs = useLoaderData();
@@ -6,6 +9,11 @@ const JobDetails = () => {
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     console.log(job);
+
+    const handleApplyJob = () => {
+        saveJobApplication(idInt);
+        toast('You have applied successfully');
+    }
     return (
         <div>
             <div className="grid md:grid-cols-4 gap-4">
@@ -17,9 +25,10 @@ const JobDetails = () => {
                 
                 <div className="border">
                     <h2 className="text-2xl">Sidebar</h2>
-                    <button className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                 </div>
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
